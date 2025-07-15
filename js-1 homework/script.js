603ed99d826798a2853d4c85431a4d29400f44ef
avazbek27 <avazbekovavazbek277@gmail.com>
2025-07-15 19:27:23 +0500

    Add files via upload

---
+++ js-1 homework/script.js
@@ -0,0 +1,20 @@
+let name = prompt("Ismin nma tvar?");
+console.log("ismi " + name + " ekan")
+let surname = prompt("Familiyenchi?");
+console.log("familiyasi " + surname + " ekan")
+let age = prompt("Yoshin nechida?");
+console.log("yoshi " + age + "da ekan")
+let school = prompt("Qaysi maktabda o'qisan?");
+console.log(school + "da o'qirkan")
+let place = prompt("Mahalla qatta?");
+console.log(place + "da yashirkan")
+let study = prompt("Qaysi yo'nalishda o'qisan?");
+console.log(study + "ga borarkan")
+let profession = prompt("Kotta bo'sen km bo'moqchisan?");
+console.log(profession + " bo'moqchikan")
+alert("tanishganmdan hursandman jonka")
+
+
+
+
+
